5a08246e1ee287ca09d6f0e8058a5ae751d88b53
Ivan Caldarella <gamein3x@gmail.com>
2026-05-12 15:37:31 +0200

    Repo aperta, react-router-dom installato, prima prova senza errori

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import {
+  BrowserRouter, Routes, Route
+} from "react-router-dom"
+
+function App() {
+  <BrowserRouter >
+    <Routes>
+      {/* https://localhost:5172/prova*/}
+      <Route path="prova"/>
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App;
