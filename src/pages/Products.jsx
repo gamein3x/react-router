b6bcd768998ada61e245a5eba609dea5ab755fc8
Ivan Caldarella <gamein3x@gmail.com>
2026-05-14 16:33:49 +0200

    Inseriti setting per i prodotti in Products con useState

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function Products() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     return (
         <div>Products</div>
-    )
+    );
 }
 export default Products
