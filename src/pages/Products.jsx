b5612cc27847cd97bad2eb51d7a601631d8a09fb
Ivan Caldarella <gamein3x@gmail.com>
2026-05-14 17:00:53 +0200

    Prima forma della fetch

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
+const API_URL = 'https://fakestoreapi.com/products';
+
 function Products() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch(API_URL)
+            // Invio errore con throw se la fetch fallisce
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Errore HTTP! Stato: ${response.status}`);
+                }
+
+                return response.json();
+            }
+            )
+    })
 
     return (
         <div>Products</div>
